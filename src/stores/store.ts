fbe856f893ec9a0c07b428011a3a1647b3408b80
yusuf <yusufpirejman@gmail.com>
2023-06-13 12:58:07 +0300

    store

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,29 +1,44 @@
-import { ref, computed } from 'vue'
+
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  const comicsData = ref([])
-  const favoriteCount = ref(0)
+interface Comic {
+  id: number;
+  favorite: boolean;
+  // Other properties
+}
 
-  function increment() {
-    count.value++
-  }
+interface CounterState {
+  count: number;
+  // doubleCount: number;
+  comicsData: Comic[];
+  favoriteCount: number;
+}
 
-  function updateComicsData(data) {
-    comicsData.value = data
-  }
-
-  function addFavorite(comic) {
-    if (comic.favorite) {
-      favoriteCount.value--
-    } else {
-      favoriteCount.value++
-    }
-    comic.favorite = !comic.favorite
-    // You can add your logic to store the favorite comic here
-  }
-
-  return { count, doubleCount, comicsData, favoriteCount, increment, updateComicsData, addFavorite }
+export const useCounterStore = defineStore('counter', {
+  state: (): CounterState => ({
+    count: 0,
+    // doubleCount: 0,
+    comicsData: [],
+    favoriteCount: 0,
+  }),
+/*   getters: {
+    doubleCount: state => state.count * 2,
+  }, */
+  actions: {
+    increment() {
+      this.count++
+    },
+    updateComicsData(data: Comic[]) {
+      this.comicsData = data
+    },
+    addFavorite(comic: Comic) {
+      if (comic.favorite) {
+        this.favoriteCount--
+      } else {
+        this.favoriteCount++
+      }
+      comic.favorite = !comic.favorite
+      // You can add your logic to store the favorite comic here
+    },
+  },
 })
